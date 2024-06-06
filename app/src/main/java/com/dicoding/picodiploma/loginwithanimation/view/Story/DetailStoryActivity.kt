f2ad9f1fe7757584e64598583bd4d1acaf671a2e
Ramadhani Putra <putrapangestu3355@gmail.com>
2024-06-06 23:12:21 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/Story/DetailStoryActivity.kt
@@ -0,0 +1,19 @@
+package com.dicoding.picodiploma.loginwithanimation.view.Story
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.picodiploma.loginwithanimation.R
+import com.dicoding.picodiploma.loginwithanimation.databinding.ActivityDetailStoryBinding
+
+class DetailStoryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailStoryBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailStoryBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_story)
+    }
+}
